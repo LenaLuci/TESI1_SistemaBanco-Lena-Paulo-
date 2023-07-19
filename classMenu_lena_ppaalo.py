9b23d47179da0ca4c955dd485548068cfc980059
Leona Luciféria <138539605+LenaLuci@users.noreply.github.com>
2023-07-19 09:54:35 -0500

    Bank patch
    Added a few lines from laboratory class and a few tweaks

--- classMenu_lena_ppaalo.py
+++ classMenu_lena_ppaalo.py
@@ -4,6 +4,10 @@ from PIL import Image, ImageTk
 
 class MenuPrincipal:
 
+    def limpar_tela(self):
+        for widget in self.janela_menu.winfo_children():
+            widget.pack_forget()
+
     def cadastrar(self):
         self.janela_menu.deiconify()
         import classConta_lena_ppaalo
@@ -12,25 +16,32 @@ class MenuPrincipal:
         self.janela_menu.deiconify()
         import classBanco_lena_ppaalo
 
+    def disable_event(self):
+        pass
+
     def __init__(self, master: Tk):
         self.janela_menu = master
         self.janela_menu.title('Menu de opções')
-        self.janela_menu.geometry('300x300')
+        self.janela_menu.geometry('350x350')
         self.janela_menu.resizable(width=False, height=False)
 
         self.frame_principal = Frame(width=300, height=300, bg='#3366cc')
         self.frame_principal.pack(fill=BOTH, expand=True)
 
-        caminho_imagem = Image.open('poggiebank logo 250x250.png')
-        imagem = caminho_imagem.resize((200, 200))
-        imagem = ImageTk.PhotoImage(imagem)
-        lbl = Label(self.frame_principal, image=imagem)
-        lbl.place(x=50, y=20)
+        self.imagem = PhotoImage(file='poggiebank logo 250x250.png')
+        imagem_red = self.imagem.subsample(3, 3)
+        lbl = Label(self.frame_principal, image=self.imagem, bg='#3366cc')
+        lbl.place(x=60, y=20)
+        #caminho_imagem = Image.open('../poggiebank logo 250x250.png')
+        #imagem = caminho_imagem.resize((200, 200))
+        #imagem = ImageTk.PhotoImage(imagem)
+        #lbl = Label(self.frame_principal, image=imagem)
+        #lbl.place(x=50, y=20)
 
         lbl_titulobanco = Label(text='PoggieBank', font='Arial', bg='#3366cc', fg='white')
-        lbl_titulobanco.place(x=105, y=225)
-        lbl_sloganbanco = Label(text='O porquinho virtual mais Pog do mundo', font='Arial, 10', bg='#3366cc', fg='white')
-        lbl_sloganbanco.place(x=37, y=250)
+        lbl_titulobanco.place(x=125, y=275)
+        lbl_sloganbanco = Label(text='O cofrinho virtual mais Pog do mundo', font='Arial, 10', bg='#3366cc', fg='white')
+        lbl_sloganbanco.place(x=62, y=300)
 
         # Adicionando o menu para selecionar as outras janelas
         self.menu_barra = Menu(self.janela_menu)
@@ -44,8 +55,12 @@ class MenuPrincipal:
 
         self.janela_menu.config(menu=self.menu_barra)
 
+        self.janela_menu.protocol("WM_DELETE_WINDOW", self.disable_event)
+
 
 app = Tk()
 janela = MenuPrincipal(app)
 app.wm_iconbitmap('poggiebank.ico')
+
+
 app.mainloop()
